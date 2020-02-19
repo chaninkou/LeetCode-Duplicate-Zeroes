7793bbd61dc2bb2bb4ebb246231b54504dc9a564
chaninkou <michaelkoulove@yahoo.com>
2020-02-18 16:20:39 -0800

    Ok problem on using two pointers to replace element next to 0 to 0.

--- src/duplicateZero/Main.java
+++ src/duplicateZero/Main.java
@@ -1,5 +1,17 @@
 package duplicateZero;
 
-public class Main {
+import java.util.Arrays;
 
+public class Main {
+	public static void main(String[] args) {
+		int[] input = {1,0,2,3,0,4,5,0};
+		
+		System.out.println("Input: " + Arrays.toString(input));
+		
+		DuplicateAllZeroesRightOneFunction solution = new DuplicateAllZeroesRightOneFunction();
+		
+		solution.duplicateZeros(input);
+		
+		System.out.println("Solution: " + Arrays.toString(input));
+	}
 }
